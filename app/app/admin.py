4c85d2af86cb7c5efdc2800c93c372fcce63d85e
Walder Kneip Filho <kfwalder@gmail.com>
2025-06-15 00:40:19 -0300

    atualizando algumas coisas

--- app/app/admin.py
+++ app/app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ponto  # Import the Ponto model
 
 # Register your models here.
+
+@admin.register(Ponto)
+class Ponto(admin.ModelAdmin):
+    list_display = ('id', 'ponto', 'local', 'tipo', 'dimensao')  # O que vai aparecer na listagem
+    search_fields = ('ponto', 'local', 'endereco')              # Para facilitar buscas
+
--- app/app/models.py
+++ app/app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Ponto(models.Model):
+    TIPO_CHOICES = [
+        ('AEROPORTO', 'AEROPORTO'),
+        ('BUSDOOR', 'BUSDOOR'),
+        ('CONTAINERS', 'CONTAINERS'),
+        ('EMPENAS', 'EMPENAS'),
+        ('FRONT-LIGHT', 'FRONT-LIGHT'),
+        ('MEGA-PAINEL', 'MEGA-PAINEL'),
+        ('MOBILIÁRIO URBANO', 'MOBILIÁRIO URBANO'),
+        ('OUTDOOR', 'OUTDOOR'),
+        ('PAINÉIS RODOVIÁRIO', 'PAINÉIS RODOVIÁRIO'),
+        ('PAINEL DE LED', 'PAINEL DE LED'),
+        ('PLACA DE ESQUINA', 'PLACA DE ESQUINA'),
+        ('TOP-SIGHT', 'TOP-SIGHT'),
+        ('TOTEM / TOTEM DIGITAL', 'TOTEM / TOTEM DIGITAL'),
+    ]
+
+    tipo = models.CharField(
+        max_length=30,
+        choices=TIPO_CHOICES
+    )
+    
+    ponto = models.CharField(max_length=20)
+    local = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=255)
+    dimensao = models.CharField(max_length=20)
+    link = models.URLField()
+    latitude = models.CharField(max_length=20)
+    longitude = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.ponto} - {self.local}'
+    
